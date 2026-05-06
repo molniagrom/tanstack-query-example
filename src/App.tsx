4c8b0ea4266e930db03289cf0a4798d8d44b3a47
molnia_grom <mgrom250@gmail.com>
2026-05-06 19:34:16 +0300

    1h 17min

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,42 @@
-import {useEffect} from 'react'
+// import {useEffect} from 'react'
 
 import './App.css'
+import {useQuery} from "@tanstack/react-query";
 import {client} from "./shared/api/client.ts";
+import {useEffect, useState} from "react";
+// import {client} from "./shared/api/client.ts";
 
 function App() {
 
+    const [isVisible, setVisible] = useState(false);
+
     useEffect(() => {
-        (async function () {
-            const res = await client.GET("/playlists")
-            const data = res.data
-            console.log(data)
-        })()
-    }, []);
+        setInterval(() => {
+            setVisible(prev => !prev);
+        }, 10000)
+    }, [])
 
     return (
         <>
-            Hello, it-incubator
+            <h2>Hello, it-incubator</h2>
+            {isVisible && <Playlists/>}
         </>
     )
 }
 
+const Playlists = () => {
+    const query = useQuery({
+        staleTime: 20000,
+        // gcTime: 5 * 1000,
+        queryKey: ['playlists'],
+        queryFn: () => client.GET("/playlists")
+    })
+    return (
+        <div>
+            {query.data?.data?.data.map((playlist) => (
+                <li>{playlist.attributes.title}</li>
+            ))}
+        </div>
+    )
+}
 export default App
