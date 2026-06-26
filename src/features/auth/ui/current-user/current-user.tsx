c89b0d13060310e9afd1cdd8068b04e2b466267a
molnia_grom <mgrom250@gmail.com>
2026-06-27 00:26:35 +0300

    refactor: remove unused button

--- src/features/auth/ui/current-user/current-user.tsx
+++ src/features/auth/ui/current-user/current-user.tsx
@@ -1,4 +1,3 @@
-import {Link} from "@tanstack/react-router";
 import styles from '../account-bar.module.css'
 import {useMeQuery} from "../../api/use-me-query.ts";
 import {LogoutButton} from "../logout-button.tsx";
@@ -11,9 +10,6 @@ export const CurrentUser = () => {
 
     return (
         <div className={styles.meInfoContainer}>
-            <Link to="/user-profile/my-playlists" activeOptions={{exact: true}} className={styles.userLink}>
-                {query.data!.login}
-            </Link>
             <LogoutButton/>
         </div>
     )
